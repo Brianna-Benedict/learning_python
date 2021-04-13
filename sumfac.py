33cfd7928f0bf4b6b2b6017607e850830ac4be31
Brianna-Benedict <brianna.a.benedict@gmail.com>
2021-04-13 00:01:06 -0700

    initial commit

---
+++ sumfac.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Write a program that computes the running sum from 1..n
+# Also, have it compute the factorial of n while you're at it
+# No, you may not use math.factorial()
+# Use the same loop for both calculations
+
+n = 5
+
+# your code goes here
+i = 0
+i = i + 1
+i += 1
+print(i)
+
+"""
+python3 sumfac.py
+5 15 120
+"""
